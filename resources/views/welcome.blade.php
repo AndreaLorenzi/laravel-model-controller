00a171e1d16070a5131f5d705194d9517191203c
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-26 23:12:15 +0200

    add image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,22 @@
         <div class="container">
             <div class="row row-cols-3">
                 @foreach ($movies as $movie)
-                    <div class="col">
+                    <div class="col py-3 h-100">
 
-                            <div class="card" style="width: 18rem;">
-                                <div class="card-body">
-                                <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}</h6>
-                                <p class="card-text">Nazionalità: {{ $movie->nationality}}</p>
-                                <p class="card-text">Data: {{ $movie->date}}</p>
-                                <p class="card-text">Voto: {{ $movie->vote}}</p>
-                                <a href="#" class="card-link">Go to Movie</a>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <img class="h-100" src="{{ Vite::asset('public/images/'. $movie->image) }}" class="card-img-top" alt="...">
+                                </div>
+                                <div class="flip-card-back">
+                                    <h5 class="card-title">Titolo: {{ $movie->title }}</h5>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movie->original_title }}</h6>
+                                    <p class="card-text">Nazionalità: {{ $movie->nationality}}</p>
+                                    <p class="card-text">Data: {{ $movie->date}}</p>
+                                    <p class="card-text">Voto: {{ $movie->vote}}</p>
                                 </div>
                             </div>
+                        </div>
                     </div>
                 @endforeach
             </div>
