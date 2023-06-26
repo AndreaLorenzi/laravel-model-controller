b9aed856835ba8e4ad47e7b80c0ca51f1287ed59
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-26 22:42:52 +0200

    stampa film con lista card

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Guest;
 
-use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('welcome', compact('movies'));
+    }
 }
+
